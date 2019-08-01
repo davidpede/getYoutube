74a50cddbf2985bc7b18bcdacab4ade412a4d224
David Pede <davidpede@users.noreply.github.com>
2019-08-01 14:46:36 +0100

    Send referer with curl request
    Adds support for HTTP_REFERER restricted keys

--- core/components/getyoutube/model/getyoutube/search.class.php
+++ core/components/getyoutube/model/getyoutube/search.class.php
@@ -35,6 +35,7 @@ class Search {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_REFERER, $this->modx->getOption('site_url'));
     curl_setopt($ch, CURLOPT_URL, $url);
     $data = curl_exec($ch);
     curl_close($ch);
